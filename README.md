076447dde76348059c9d08244b349842e80ca323
pankaj.mest@gmail.com <pankaj.mest@gmail.com>
2017-11-03 23:02:38 -0700

    preprocessing initial commit

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # chatbot
 train a neural net to answer specific question about class schedule and information 
+
+### A bit about word2vec
+
+There are a number of parameter choices that affect the run time and the quality of the final model that is produced. For details on the algorithms below, see the word2vec API documentation as well as the Google documentation. 
+
+* Architecture: Architecture options are skip-gram (default) or continuous bag of words. We found that skip-gram was very slightly slower but produced better results.
+* Training algorithm: Hierarchical softmax (default) or negative sampling. For us, the default worked well.
+* Downsampling of frequent words: The Google documentation recommends values between .00001 and .001.
+* Word vector dimensionality: More features result in longer runtimes, and often, but not always, result in better models. Reasonable values can be in the tens to hundreds; we used 300.
+* Context / window size: How many words of context should the training algorithm take into account? 10 seems to work well for hierarchical softmax (more is better, up to a point).
+* Worker threads: Number of parallel processes to run. This is computer-specific, but between 4 and 6 should work on most systems.
+* Minimum word count: This helps limit the size of the vocabulary to meaningful words. Any word that does not occur at least this many times across all documents is ignored. Reasonable values could be between 10 and 100.
+
+### Preprocessing.py has methods to preprocess the texts, train word2vec and vectorize the train and test sets.
+
+1.text_to_wordlist(text, remove_stopwords=False)
+  returns a list of processed word in a sentence.
+  processing includes: removing numbers and special characters, converting them into lowercase, 
+  removing stopwords(optional), stemming and lemmatizing.
+
+2.text_to_sentences(text, tokenizer,remove_stopwords=False )
+  returns a list of processed sentences in a document. tokenizer is the tokenizer used in this case it is punkt tokenizer
+
+3.parse_and_clean_sentences(df)
+  returns all the processed sentences in all the documents. df is a pandas dataframe. df should contain df["text"] as the document column name
+
+4.train_word2vec(df, num_features = 300, min_word_count = 1, num_workers = 4, context = 4, downsampling = 1e-3)
+  trains word to vec with df["text"] as input data. Saves and returns the model.
+  the default parameter values are:
+
+  num_features = 300    # Word vector dimensionality
+  min_word_count = 1    # Minimum word count
+  num_workers = 4       # Number of threads to run in parallel
+  context = 4           # Context window size
+  downsampling = 1e-3   # Downsample setting for frequent words
+
+5.makeFeatureVec(words, model, num_features)
+  counts the average word vectors of a document 
+
+6.getAvgFeatureVecs(text, model)
+  Given a set of documents (each one a list of words), calculate the average feature vector for each one and return a 2D numpy array 
+
+7.preprocess_data(df, model)
+  basically this function does everything :) just call this function to return processed and vectorized data.
+
+Follow this order to train wordd2vec and then vectorize the train and test data
+* import dataset
+df = pd.read_csv("datafile.csv", header=0, delimiter="\t", quoting=3)
+
+* train word2vec dataset
+model = train_word2vec(df, num_features, min_word_count, num_workers, context, downsampling)
+
+* preprocess and vectorize train and test data
+train_data = preprocess_data(trainDataFrame, model)
+test_data = preprocess_data(testDataFrame, model)
+
+some useful calls
+
+//from gensim.models import Word2Vec ------ import w2v
+//model = train_word2vec(df, num_features, min_word_count, num_workers, context, downsampling) ---- to train and save model
+//model.most_similar("hi") -------- find similar words to a word
+//model = Word2Vec.load("trainedWord2vecmodel") ------ load the saved model model
+//model.wv.syn0.shape --------- find shape of the model
+//model.wv.index2word -------- all the words in the model
+//model["word"] ------- features of the word
+
---
+++ preprocessing.py
@@ -0,0 +1,198 @@
+
+# coding: utf-8
+
+# In[138]:
+
+
+from autocorrect import spell
+import pandas as pd
+from nltk.corpus import stopwords # Import the stop word list
+from nltk.stem import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
+import re
+import nltk.data
+import logging
+import numpy as np
+
+
+# In[111]:
+
+
+stop = stopwords.words("english")
+wordnet_lemmatizer = WordNetLemmatizer()
+snowball_stemmer = SnowballStemmer("english")
+# Load the punkt tokenizer
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+
+# In[101]:
+
+
+def text_to_wordlist(text, remove_stopwords=False):
+    # 1. Remove non-letters        
+    letters_only = re.sub("[^a-zA-Z]", " ", text) 
+    #
+    # 2. Convert to lower case, split into individual words
+    words = letters_only.lower().split() 
+    #
+    # 3. In Python, searching a set is much faster than searching
+    #    a list, so convert the stop words to a set
+    stops = set(stopwords.words("english"))
+    # 
+    # 4. Remove stop words
+    if remove_stopwords:
+        meaningful_words = [w for w in words if not w in stops]  
+    else:
+        meaningful_words = words
+    # 
+    # 5. autocorrect spellings
+    #auto_correct = [spell(w) for w in meaningful_words]
+    # 
+    # 6. use stemmer to stem
+    stem_words = [snowball_stemmer.stem(w) for w in meaningful_words]
+    #
+    # 7. use lemmatizer to lemmatize the words
+    lemma_words = [wordnet_lemmatizer.lemmatize(w) for w in stem_words]
+    #
+    # 8. Join the words back into one string separated by space, 
+    #    and return the result.
+    return(lemma_words)
+
+
+# In[103]:
+
+
+# Define a function to split a review into parsed sentences
+# we needd sentences because word2vec takes sentences as input. It leverages the SBD
+def text_to_sentences(text, tokenizer,remove_stopwords=False ):
+    # Function to split a review into parsed sentences. Returns a 
+    # list of sentences, where each sentence is a list of words
+    #
+    # 1. Use the NLTK tokenizer to split the paragraph into sentences
+    raw_sentences = tokenizer.tokenize(text.strip())
+    #
+    # 2. Loop over each sentence
+    sentences = []
+    for raw_sentence in raw_sentences:
+        # If a sentence is empty, skip it
+        if len(raw_sentence) > 0:
+            # Otherwise, call text_to_wordlist to get a list of words
+            sentences.append(text_to_wordlist(raw_sentence,remove_stopwords))
+    #
+    # Return the list of sentences (each sentence is a list of words,
+    # so this returns a list of lists
+    return sentences
+
+
+# In[107]:
+
+
+def parse_and_clean_sentences(df):
+    sentences = []  # Initialize an empty list of sentences
+
+    print ("Parsing sentences from training set")
+    for text in df["text"]:
+        sentences += text_to_sentences(text, tokenizer)
+    print ("parsing done!")   
+    return sentences
+
+
+# In[122]:
+
+
+def train_word2vec(df, num_features = 300, min_word_count = 1, num_workers = 4, context = 4, downsampling = 1e-3):
+    
+    from gensim.models import word2vec
+    # parse and clean sentence
+    sentences = parse_and_clean_sentences(df)
+    
+    #initialize logging
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',level=logging.INFO)
+
+    # Initialize and train the model (this will take some time)
+
+    print ("Training model...")
+    model = word2vec.Word2Vec(sentences, workers=num_workers,size=num_features, min_count = min_word_count,
+                              window = context, sample = downsampling)
+
+    # init_sims will make the model much more memory-efficient.
+    model.init_sims(replace=True)
+
+    # save the model for later use.can load it later using Word2Vec.load()
+    model_name = "trainedWord2vecmodel"
+    model.save(model_name)
+    print ("model saved as", model_name)
+    
+    return model
+
+
+# In[145]:
+
+
+##A simple way to assign a word2vec vector to a document is to take a mean of its words.
+def makeFeatureVec(words, model, num_features):
+    # Function to average all of the word vectors in a given
+    # paragraph
+    #
+    # Pre-initialize an empty numpy array (for speed)
+    featureVec = np.zeros((num_features,),dtype="float32")
+    #
+    nwords = 0.
+    # 
+    # Index2word is a list that contains the names of the words in 
+    # the model's vocabulary. Convert it to a set, for speed 
+    index2word_set = set(model.wv.index2word)
+    #
+    # Loop over each word in the review and, if it is in the model's
+    # vocaublary, add its feature vector to the total
+    for word in words:
+        if word in index2word_set: 
+            nwords = nwords + 1.
+            featureVec = np.add(featureVec,model[word])
+    # 
+    # Divide the result by the number of words to get the average
+    featureVec = np.divide(featureVec,nwords)
+    return featureVec
+
+
+# In[154]:
+
+
+def getAvgFeatureVecs(text, model):
+    # Given a set of documents (each one a list of words), calculate 
+    # the average feature vector for each one and return a 2D numpy array 
+    # 
+    #find the num of features
+    num_features = model.wv.syn0.shape[1]
+    
+    # Initialize a counter
+    counter = 0
+    # 
+    # Preallocate a 2D numpy array, for speed
+    reviewFeatureVecs = np.zeros((len(text),num_features),dtype="float32")
+    # 
+    # Loop through the reviews
+    for example in text:
+        # 
+        # Call the function (defined above) that makes average feature vectors
+        reviewFeatureVecs[counter] = makeFeatureVec(example, model,num_features)
+        #
+        # Increment the counter
+        counter = counter + 1
+        
+    return reviewFeatureVecs
+
+
+# In[158]:
+
+
+def preprocess_data(df, model):
+    textlist = []
+    print ("Creating average feature vecs for the input documents")
+    for text in df["text"]:
+        textlist.append(text_to_wordlist(text, remove_stopwords=True ))
+        
+    DataVecs = getAvgFeatureVecs(textlist, model)
+    print ("done!!!")
+    return DataVecs
+
