8702b923ba303247e50df2e5aa779e8677d3037a
pankaj.mest@gmail.com <pankaj.mest@gmail.com>
2017-11-21 19:55:48 -0800

    added requirements

---
+++ run.py
@@ -0,0 +1,39 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+import sys
+
+
+# In[1]:
+
+
+def train_word2vec(num_features, min_word_count, num_workers, context, downsampling):
+    from preprocessing import train_word2vec
+    import pandas as pd
+    df = pd.read_csv("datafile.csv", header=0, delimiter="\t", quoting=3)
+    model = train_word2vec(df, num_features, min_word_count, num_workers, context, downsampling)
+    return model
+
+
+# In[1]:
+
+
+def train_model():
+    import model
+
+
+# In[9]:
+
+
+if __name__ == "__main__":
+    x = 0
+    if sys.argv[1] == "word2vec":
+        train_word2vec(int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]), float(sys.argv[6]))
+    elif sys.argv[1] == "model":
+        train_model()
+    else:
+        x = 0
+
