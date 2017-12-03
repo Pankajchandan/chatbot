e01ae9c042a274cd661f99a5f5b7b64c489b4cb1
pankaj.mest@gmail.com <pankaj.mest@gmail.com>
2017-12-03 15:28:35 -0800

    app change

--- app.py
+++ app.py
@@ -6,11 +6,14 @@ from model_predict import get_response
 import requests
 from flask import Flask, request
 
+log("import libs completed")
+
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def verify():
+    
     # when the endpoint is registered as a webhook, it must echo back
     # the 'hub.challenge' value it receives in the query arguments
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
@@ -92,4 +95,5 @@ def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
 
 
 if __name__ == '__main__':
+    log("running app")
     app.run(debug=True)
